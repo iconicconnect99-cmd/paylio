771b865b8b2a6321e1853fd7ca3c6180f9d81214
Haidware <adesuyi.tobi@haidware.com>
2026-09-12 02:32:46 -0700

    Serve static assets on Render

--- payment_prj/settings.py
+++ payment_prj/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 CSRF_TRUSTED_ORIGINS = [
     origin.strip()
